07115e69abe300be22c26c59449a2077445d482f
Ryan <ryan@expansiagroup.com>
2020-03-23 14:24:42 -0600

    Adding covid19 and colon cancerquestions

--- eliza.py
+++ eliza.py
@@ -52,7 +52,7 @@ def getage():
     retval = clamp(int(age), 0, 150)
     return str(retval)
 
-age = getage() #TODO add logic to ask additional questions for those over 55
+age = getage()
 
 #TODO add logic to get user gender for gender related medical questions
 
@@ -62,7 +62,77 @@ age = getage() #TODO add logic to ask additional questions for those over 55
 eliza_speech("So your name is " + name + " and you are " + age + " years old.", "eliza_statenameandage.mp3")
 print("So your name is " + name + " and you are " + age + " years old.")
 
-eliza_speech(name + " I am going to ask some questions about your physical health.", "eliza_healthquestionsprompt.mp3")
+# Coronavirus
+def covid19check():
+    # Covid19 screening
+    eliza_speech("Have you recently experienced any of the following symptoms, fever, cough, shortness of breath, and/or breathing difficulties?", "eliza_coloncancerscreen.mp3")
+    while True:
+        covid19_screening_reply = input("Yes or No: ")
+        if covid19_screening_reply.isalpha():
+            if (covid19_screening_reply.lower() == "yes"):
+                eliza_speech(
+                    "If you have a fever, cough and difficulty breathing, seek medical attention and call in advance. Follow the directions of your local health authority" + name,
+                    "eliza_covid19yes.mp3")
+                print(
+                    "If you have a fever, cough and difficulty breathing, seek medical attention and call in advance. Follow the directions of your local health authority " + name)
+                covid19_score = 10
+
+                return covid19_score
+
+            elif (covid19_screening_reply.lower() == "no"):
+                eliza_speech(
+                    "Great, please continue to do the 5! Wash your hands often, cough into your elbow, don't touch your face, maintain social distance, and stay home if you can " + name,
+                    "eliza_getcoloncancerscreen.mp3")
+                print("Great, please continue to do the 5! Wash your hands often, cough into your elbow, don't touch your face, maintain social distance, and stay home if you can " + name)
+                covid19_score = 0
+
+                return covid19_score
+
+            elif (covid19_screening_reply.lower() != "yes" or "no"):
+                eliza_speech(name + " please reply with yes or no.", "eliza_yesnovalidation.mp3")
+        eliza_speech("Please enter alphabetic characters only.", "eliza_selfharmalphvalidation.mp3")
+        print("Please enter alphabetic characters only.")
+
+covid19_score = covid19check()
+
+# If user is over 55, ask additional questions
+def agecheck():
+    if int(age) > 55:
+        eliza_speech(name + "Because you are older than 55, I am going to ask you some additional questions.", "eliza_over55.mp3")
+        print(name + " Because you are older than 55, I am going to ask you some additional questions.")
+
+        # Colon cancer screening
+        eliza_speech(name + "Have you been screened for colon cancer in the last 5 years?", "eliza_coloncancerscreen.mp3")
+        while True:
+            colon_cancer_screening_reply = input("Yes or No: ")
+            if colon_cancer_screening_reply.isalpha():
+                if (colon_cancer_screening_reply.lower() == "yes"):
+                    eliza_speech(
+                        "Well done " + name,
+                        "eliza_welldonename.mp3")
+                    print(
+                        "Well done " + name)
+                    colon_cancer_score = 0
+                    eliza_speech("Ok thats all the age related questions I have for you " + name,
+                                 "eliza_agequestionswrapup.mp3")
+                    print("Ok that's all the age related questions I have for you " + name)
+                    return colon_cancer_score
+                elif (colon_cancer_screening_reply.lower() == "no"):
+                    eliza_speech("Colon cancer screening is recommended for everyone at age 50. The colonoscopy is a test that is most frequently recommended, though there are other options. Ask your doctor which screening test is best for you. " + name, "eliza_getcoloncancerscreen.mp3")
+                    print("That is something that everyone should look into after 50 " + name)
+                    colon_cancer_score = 2
+                    eliza_speech("Ok thats all the age related questions I have for you " + name,
+                                 "eliza_agequestionswrapup.mp3")
+                    print("Ok that's all the age related questions I have for you " + name)
+                    return colon_cancer_score
+                elif (colon_cancer_screening_reply.lower() != "yes" or "no"):
+                    eliza_speech(name + " please reply with yes or no.", "eliza_yesnovalidation.mp3")
+            eliza_speech("Please enter alphabetic characters only.", "eliza_selfharmalphvalidation.mp3")
+            print("Please enter alphabetic characters only.")
+
+colon_cancer_score = agecheck()
+
+eliza_speech("I am going to ask some questions about your physical health.", "eliza_healthquestionsprompt.mp3")
 
 eliza_speech("How do you feel physically? Enter a value between 1 and 10 with 10 meaning you feel amazing.", "eliza_howdoyoufeelphysically.mp3")
 print("How do you feel physically? Enter a value between 1 and 10 with 10 meaning you feel amazing.")
@@ -83,7 +153,7 @@ phys_score = physcheck()
 
 eliza_speech(name + " let's get your temperature. Please take your temperature and reply with the result. For example if the thermometer reads 98.5, enter 98.5", "eliza_checktemp.mp3")
 print(name + " let's get your temperature. Please take your temperature and reply with the result. For example if the thermometer reads 98.5, enter 98.5")
-
+# TODO look into apple/android watch temperature APIs
 def tempcheck(): #TODO look into thermometers with an API
     temp_reply = input("Reply: ")
     clamp(float(temp_reply), 0.0, 200.0)
@@ -107,7 +177,7 @@ temp_score = tempcheck()
 
 #TODO add logic for a blood pressure check
 
-#TODO add logic to check time stamp against users last physical
+#TODO add logic to check time stamp against users last physical. Can we gain access to medical data via an API?
 
 # Mental Health Check
 
@@ -116,6 +186,7 @@ eliza_speech(name + " Now I am going to ask some questions about your mental hea
 eliza_speech("How do you feel emotionally? Enter a value between 1 and 10 with 10 meaning you feel perfectly sound.", "eliza_howdoyoufeelemo.mp3")
 print("How do you feel emotionally? Enter a value between 1 and 10 with 10 meaning you fell perfectly sound.")
 
+# TODO Should there be a separate subjective optional path?
 def emotcheck():
     while True:
         emot_score = input("1 - 10: ")
@@ -180,7 +251,8 @@ danger_score2 = harmcheck()
 
 # Calculate Well Being Score
 def sumscore():
-    total_danger_score = int(danger_score) + int(danger_score2)
+    # TODO Refactor the scoring, needs to be clean.
+    total_danger_score = int(danger_score) + int(danger_score2) + int(colon_cancer_score) + int(covid19_score)
     total_mindbody_score = int(emot_score) + int(phys_score)
     wellbeing_score = int(total_mindbody_score) - int(total_danger_score)
     wellbeing_score_total = int(wellbeing_score) - int(temp_score)
@@ -189,9 +261,8 @@ def sumscore():
     print("Your wellness score is: " + wellbeing_score_tostring + " out of 20")
     if (wellbeing_score_total <= 10):
         if (wellbeing_score_total > 5):
-            eliza_speech(name + " your wellness score is sub-optimal, I recommend you go for a short walk to boost your well being", "eliza_suboptimalscore.mp3")
-            print(name + " your wellness score is sub-optimal, I recommend you go for a short walk to boost your well "
-                         "being.")
+            eliza_speech(name + " your wellness score is sub-optimal, I recommend you take a day of rest and increase your water intake to boost your well being. I would also suggest following up with your health care provider if you do not improve within the next seven days.", "eliza_suboptimalscore.mp3")
+            print(name + " your wellness score is sub-optimal, I recommend you take a day of rest and increase your water intake to boost your well being. I would also suggest following up with your health care provider if you do not improve within the next seven days.")
         elif (wellbeing_score_total <= 5):
             eliza_speech(name + " your wellness score is dangerously low, I recommend you speak with a loved one and/or your doctor about options to improve your well being.", "eliza_dangerscore.mp3")
             print(name + " your wellness score is dangerously low, I recommend you speak with a loved one and/or your "
@@ -206,3 +277,5 @@ def sumscore():
     return wellbeing_score_total
 
 sumscore()
+
+# TODO Look into reporting, dashboards, etc... Think heatmap of user temp data and producing fever trends on a dashboard.
